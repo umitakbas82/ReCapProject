e9e8bf5166e1498c52009a2750a8ccea07b0ccf6
umitakbas82 <umit.akbas@hotmail.com.tr>
2021-12-02 15:49:51 +0300

    CarRentalProject

---
+++ Entities/Concrete/Car.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entities.Concrete
+{
+    public class Car
+    {
+
+        public int CarID { get; set; }
+        public int BrandID { get; set; }
+        public int ColourID { get; set; }
+        public int ModelYear { get; set; }
+        public decimal DailyPrice { get; set; }
+        public string Description { get; set; }
+
+    }
+}
